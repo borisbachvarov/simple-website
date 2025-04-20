da88e68af022708b9ba03a08645fe38a67ed69f0
Boris <boris.bachvarov@pytek.bg>
2025-04-20 13:27:15 +0300

    added translation and some design touches

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navbar = document.querySelector('.navbar');
-    window.addEventListener('scroll', function () {
-      if (window.scrollY > 10) {
-        navbar.classList.add('navbar-scrolled');
-      } else {
-        navbar.classList.remove('navbar-scrolled');
-      }
-    });
+  const navbar = document.querySelector('.navbar');
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 10) {
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
   });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".nav-link[href^='#']");
@@ -19,4 +19,15 @@ document.addEventListener("DOMContentLoaded", function () {
       this.classList.add("active");
     });
   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const langToggle = document.getElementById("language-toggle");
+  const currentPath = window.location.pathname;
+
+  if (currentPath.includes("/en/")) {
+    langToggle.textContent = "EN";
+  } else {
+    langToggle.textContent = "БГ";
+  }
 });
