fe2b4561629e140c7546349bcbc3b595a3e971c2
Boris <boris.bachvarov@pytek.bg>
2025-04-20 11:05:18 +0300

    Initial commit - project setup

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 10) {
+        navbar.classList.add('navbar-scrolled');
+      } else {
+        navbar.classList.remove('navbar-scrolled');
+      }
+    });
+  });
